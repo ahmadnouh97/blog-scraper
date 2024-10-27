c2011e2db8928985b3bc2ca67af4542f01f9b737
Ahmad Nouh <ahmadnouh428@gmail.com>
2024-10-27 13:43:00 +0300

    chore: update response body

--- scraper/devto.go
+++ scraper/devto.go
@@ -6,33 +6,54 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"time"
 
 	md "github.com/JohannesKaufmann/html-to-markdown"
 	"github.com/ahmadnouh97/blog-scraper/utils"
 )
 
 type DevToData struct {
-	ClassName                string        `json:"class_name"`
-	CloudinaryVideoURL       interface{}   `json:"cloudinary_video_url"`
-	ID                       int           `json:"id"`
-	Path                     string        `json:"path"`
-	PublicReactionsCount     int           `json:"public_reactions_count"`
-	ReadablePublishDate      string        `json:"readable_publish_date"`
-	ReadingTime              int           `json:"reading_time"`
-	Title                    string        `json:"title"`
-	Content                  string        `json:"content"`
-	UserID                   int           `json:"user_id"`
-	PublicReactionCategories []interface{} `json:"public_reaction_categories"`
-	CommentsCount            int           `json:"comments_count"`
-	VideoDurationString      string        `json:"video_duration_string"`
-	PublishedAtInt           int           `json:"published_at_int"`
-	TagList                  []interface{} `json:"tag_list"`
-	FlareTag                 interface{}   `json:"flare_tag"`
-	User                     struct {
-		Name           string `json:"name"`
-		ProfileImage90 string `json:"profile_image_90"`
-		Username       string `json:"username"`
+	TypeOf                 string      `json:"type_of"`
+	ID                     int         `json:"id"`
+	Title                  string      `json:"title"`
+	Description            string      `json:"description"`
+	Content                string      `json:"content"`
+	CoverImage             string      `json:"cover_image"`
+	ReadablePublishDate    string      `json:"readable_publish_date"`
+	SocialImage            string      `json:"social_image"`
+	TagList                []string    `json:"tag_list"`
+	Tags                   string      `json:"tags"`
+	Slug                   string      `json:"slug"`
+	Path                   string      `json:"path"`
+	URL                    string      `json:"url"`
+	CanonicalURL           string      `json:"canonical_url"`
+	CommentsCount          int         `json:"comments_count"`
+	PositiveReactionsCount int         `json:"positive_reactions_count"`
+	PublicReactionsCount   int         `json:"public_reactions_count"`
+	CollectionID           interface{} `json:"collection_id"`
+	CreatedAt              time.Time   `json:"created_at"`
+	EditedAt               time.Time   `json:"edited_at"`
+	CrosspostedAt          interface{} `json:"crossposted_at"`
+	PublishedAt            time.Time   `json:"published_at"`
+	LastCommentAt          time.Time   `json:"last_comment_at"`
+	PublishedTimestamp     time.Time   `json:"published_timestamp"`
+	ReadingTimeMinutes     int         `json:"reading_time_minutes"`
+	User                   struct {
+		Name            string `json:"name"`
+		Username        string `json:"username"`
+		TwitterUsername string `json:"twitter_username"`
+		GithubUsername  string `json:"github_username"`
+		WebsiteURL      string `json:"website_url"`
+		ProfileImage    string `json:"profile_image"`
+		ProfileImage90  string `json:"profile_image_90"`
 	} `json:"user"`
+	Organization struct {
+		Name           string `json:"name"`
+		Username       string `json:"username"`
+		Slug           string `json:"slug"`
+		ProfileImage   string `json:"profile_image"`
+		ProfileImage90 string `json:"profile_image_90"`
+	} `json:"organization"`
 }
 
 func FetchBlogs(params map[string]string) ([]*DevToData, error) {
