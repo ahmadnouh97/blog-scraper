6531af51fe6b47bbdbf357fc5171b7f7fe90ef32
Ahmad Nouh <ahmadnouh428@gmail.com>
2024-10-28 01:14:35 +0300

    feat: update table

--- db/migrations/20241027112412_blogs.sql
+++ db/migrations/20241027112412_blogs.sql
@@ -1,15 +1,39 @@
 -- +goose Up
--- +goose StatementBegin
-CREATE TABLE IF NOT EXISTS blogs (
+CREATE TABLE blogs (
     id INTEGER PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
+    title TEXT NOT NULL,
     content TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+    description TEXT,
+    cover_image TEXT,
+    readable_publish_date TEXT,
+    social_image TEXT,
+    tag_list TEXT,
+    tags TEXT,
+    slug TEXT,
+    path TEXT,
+    url TEXT,
+    canonical_url TEXT,
+    comments_count INTEGER DEFAULT 0,
+    positive_reactions_count INTEGER DEFAULT 0,
+    public_reactions_count INTEGER DEFAULT 0,
+    collection_id INTEGER,
+    created_at TEXT NOT NULL DEFAULT (datetime('now')),
+    edited_at TEXT,
+    published_at TEXT,
+    last_comment_at TEXT,
+    published_timestamp TEXT,
+    reading_time_minutes INTEGER,
+    username TEXT,
+    user_full_name TEXT,
+    user_profile_image TEXT,
+    user_profile_image_90 TEXT,
+    organization_name TEXT,
+    organization_username TEXT,
+    organization_profile_image TEXT,
+    organization_profile_image_90 TEXT,
+    organization_slug TEXT,
+    type_of TEXT
 );
 
--- +goose StatementEnd
-
 -- +goose Down
--- +goose StatementBegin
-Drop table blogs;
--- +goose StatementEnd
+DROP TABLE IF EXISTS blogs;
